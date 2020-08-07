e710799008f74139a1973978ed7e0d8a65bd5881
Maria <mariachernovolos@gmail.com>
2020-08-08 01:58:58 +0300

    added GoodCategory model;

---
+++ src/Mocks/GoodsCategory.js
@@ -0,0 +1,26 @@
+import {v4 as uuidv4} from 'uuid';
+
+export const goodsCategory = [
+    {
+        id: uuidv4(),
+        name: 'Soft drinks',
+    },
+    {
+        id: uuidv4(),
+        name: 'Alcohol',
+    },
+    {
+        id: uuidv4(),
+        name: 'Fruits',
+    },
+    {
+        id: uuidv4(),
+        name: 'Vegetables',
+    },
+    {
+        id: uuidv4(),
+        name: 'Meat',
+    },
+];
+
+
