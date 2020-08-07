8932e46d27659ed906ed5bcff796ade02298d5d2
Maria <mariachernovolos@gmail.com>
2020-08-08 02:03:07 +0300

    added onToggle, onEdit, onDeleteSelected, addNewItem, getSubtotal,getTotal,removeElementById,getEditElement,deleteSelectedElements,toggleSelectedGood,removeSelectedGoodById functions;

--- src/App/App.js
+++ src/App/App.js
@@ -1,54 +1,150 @@
-import React, { Component } from 'react'
-
-import './App.css'
-
-import GoodsList from '../GoodsList/GoodsList'
-import { goods } from '../Mocks/GoodsMock'
-import GoodsListForm from '../GoodsListForm/GoodsListForm'
-import { addNewItem, removeElementById, getTotal } from '../Utils/goodsUtils'
+import React, {Component} from 'react';
+import './App.css';
+import {goods} from '../Mocks/GoodsMock';
+import {goodsCategory} from '../Mocks/GoodsCategory';
+import GoodsList from '../GoodsList/GoodsList';
+import GoodsListForm from '../GoodsListForm/GoodsListForm';
+import {
+    addNewItem,
+    getSubtotal,
+    getTotal,
+    removeElementById,
+    getEditElement,
+    deleteSelectedElements,
+    toggleSelectedGood,
+    removeSelectedGoodById,
+} from '../Utils/goodsUtils';
+import PropTypes from 'prop-types';
 
 export default class App extends Component {
-  state = {
-    goods,
-    total: getTotal(goods),
-  }
+    state = {
+        goods,
+        goodsCategory,
+        total: getTotal(goods),
+        subtotalElement: 0,
+        selectedGoods: [],
+    };
 
-  recalculateTotal = () => {
-    this.setState((state) => ({
-      total: getTotal(state.goods)
-    }))
-  }
+    recalculateTotal = () => {
+        this.setState((state) => ({
+            ...this.state,
+            total: getTotal(state.goods),
+        }));
+    };
 
-  onAdd = (newElement) => {
-    this.setState(({ goods }) => {
-      const newArray = addNewItem(newElement, goods)
-      return {
-        goods: newArray,
-        total: getTotal(newArray),
-    }})
-  }
+    onAdd = (newElement) => {
+        this.setState(({goods}) => {
+            const newArray = addNewItem(newElement, goods);
+            return {
+                ...this.state,
+                goods: newArray,
+                subtotalElement: getSubtotal(newArray, this.state.selectedGoods),
+                total: getTotal(newArray),
+            };
+        });
+    };
 
-  onDelete = (id) => {
-    const newArray = removeElementById(id, this.state.goods)
-    this.setState({
-      goods: newArray,
-      total: getTotal(newArray),
-    })
-  }
+    onDelete = (id) => {
+        const newArray = removeElementById(id, this.state.goods);
+        const newSelectedGoods = removeSelectedGoodById(this.state.selectedGoods, id);
+        this.setState({
+            ...this.state,
+            goods: newArray,
+            selectedGoods: newSelectedGoods,
+            subtotalElement: getSubtotal(newArray, newSelectedGoods),
+            total: getTotal(newArray),
+        });
+    };
 
-  render() {
-    const { total, goods } = this.state
-    return (
-      <div className="Container">
-        <div className="Title">Fridge</div>
-        <GoodsList goods={goods} onDelete={this.onDelete}/>
-        <div className="Total">
-          <div>Total:</div>
-          <div>{total}</div>
-          <div/>
-        </div>
-        <GoodsListForm onAdd={this.onAdd}/>
-      </div>
-    )
-  }
+    onToggle = (id) => {
+        const newSelectedGoods = toggleSelectedGood(this.state.selectedGoods, id);
+        this.setState({
+            ...this.state,
+            selectedGoods: newSelectedGoods,
+            subtotalElement: getSubtotal(this.state.goods, newSelectedGoods),
+        });
+    };
+
+    onEdit = (updatedGood) => {
+        const updatedGoods = getEditElement(this.state.goods, updatedGood);
+        this.setState({
+            ...this.state,
+            goods: updatedGoods,
+        });
+    };
+
+    onDeleteSelected = () => {
+        const newArray = deleteSelectedElements(this.state.goods, this.state.selectedGoods);
+        this.setState({
+            ...this.state,
+            goods: newArray,
+            selectedGoods: [],
+            subtotalElement: getSubtotal(newArray, []),
+            total: getTotal(newArray),
+        });
+    };
+
+    render() {
+        const {total, goods, subtotalElement, goodsCategory, selectedGoods} = this.state;
+        return (
+            <div className="container">
+                <div>
+                    <div className="Title">Fridge</div>
+                </div>
+                <section>
+                    <div className="AppWrapper">
+                        <GoodsListForm onAdd={this.onAdd} goodsCategory={goodsCategory}/>
+                        <div className="TotalWrapper">
+                            <div className="Total">
+                                <h3>Subtotal: </h3>
+                                <p className="TotalNumber">{subtotalElement}</p>
+                            </div>
+                            <div className="Total">
+                                <h3>Total: </h3>
+                                <p className="TotalNumber">{total}</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="AppButtonWrapper">
+                        <button
+                            onClick={this.onDeleteSelected}
+                            disabled={!selectedGoods.length}
+                            className="button AppButton"
+                        >
+                            delete
+                        </button>
+                    </div>
+                </section>
+                <GoodsList
+                    goods={goods}
+                    selectedGoods={selectedGoods}
+                    onDelete={this.onDelete}
+                    onToggle={this.onToggle}
+                    onEdit={this.onEdit}
+                    onDeleteSelected={this.onDeleteSelected}
+                />
+            </div>
+        );
+    }
 }
+
+App.defaultProps = {
+    goods: [],
+    selectedElement: [],
+    total: 0,
+    subtotalElement: 0,
+    goodsCategory: [],
+};
+
+App.propTypes = {
+    total: PropTypes.number,
+    subtotalElement: PropTypes.number,
+    goods: PropTypes.array,
+    selectedGoods: PropTypes.array,
+    goodsCategory: PropTypes.array,
+    onAdd: PropTypes.func,
+    onDelete: PropTypes.func,
+    onToggle: PropTypes.func,
+    onEdit: PropTypes.func,
+    onDeleteSelected: PropTypes.func,
+};
--- src/Utils/goodsUtils.js
+++ src/Utils/goodsUtils.js
@@ -1,22 +1,77 @@
-import { v4 as uuidv4 } from 'uuid';
+import {v4 as uuidv4} from 'uuid';
 
 export const newItemFromData = (data) => {
     return {
         id: uuidv4(),
         ...data,
-    }
-}
+    };
+};
 
 export const addNewItem = (data, goods) => {
-    return [...goods, newItemFromData(data)]
-}
+    return [...goods, newItemFromData(data)];
+};
 
 export const removeElementById = (id, goods) => {
-    return goods.filter((e) => e.id !== id)
-}
+    return goods.filter((e) => e.id !== id);
+};
 
 export const getTotal = (goods) => {
     return goods.reduce((acc, item) => {
         return acc + parseFloat(item.weight);
-    }, 0)
-}
+    }, 0);
+};
+
+
+export const getToggleElement = (id, goods) => {
+    console.log(goods, id);
+    return goods.map((good) => {
+        if (good.id === id) {
+            good.selected = !good.selected;
+        }
+        return good;
+    });
+};
+
+export const toggleSelectedGood = (selectedGoods, id) => {
+    if (selectedGoods.indexOf(id) !== -1) {
+        selectedGoods.splice(selectedGoods.indexOf(id), 1);
+    } else {
+        selectedGoods.push(id);
+    }
+    return selectedGoods;
+};
+
+export const getSubtotal = (goods, selectedGoods) => {
+    return goods.reduce((acc, good) => {
+        if (selectedGoods.indexOf(good.id) !== -1) {
+            return acc + parseFloat(good.weight);
+        }
+        return acc;
+    }, 0);
+};
+
+export const getEditElement = (goods, updatedGood ) => {
+    return goods.map((good) => {
+        if (good.id === updatedGood.id) {
+            return updatedGood;
+        } else {
+            return good;
+        }
+    });
+};
+
+export const deleteSelectedElements = (goods, selectedGoods) => {
+    for (let i = 0; i < goods.length; i++) {
+        if (selectedGoods.indexOf(goods[i].id) !== -1) {
+            goods.splice(i, 1);
+        }
+    }
+    return goods;
+};
+
+export const removeSelectedGoodById = (arr, id) => {
+    if (arr.indexOf(id) > -1) {
+        arr.splice(arr.indexOf(id), 1);
+    }
+    return arr;
+};
