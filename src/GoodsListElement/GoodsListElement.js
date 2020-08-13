68e22708ae714e9271b627288e79bf859c7d3fff
Maria <mariachernovolos@gmail.com>
2020-08-14 00:35:33 +0300

    converted to functional component GoodListElement;

--- src/GoodsListElement/GoodsListElement.js
+++ src/GoodsListElement/GoodsListElement.js
@@ -1,67 +1,89 @@
-import React, {Component} from 'react';
+import React, {useState, useCallback} from 'react';
 import './GoodsListElement.css';
 import PropTypes from 'prop-types';
 
-export default class GoodsListElement extends Component {
-    state = {
+export default function GoodsListElement(props) {
+    const {good, isSelected, onDelete, onToggle, onEdit} = props;
+
+    const [stateElement, setStateElement] = useState({
         isActive: false,
         isEditMode: false,
         title: '',
         weight: '',
         description: '',
-    };
+    });
 
-    onDelete = () => {
-        this.props.onDelete(this.props.good.id);
-        console.log(this.props.good.id);
-    };
+    const onDeleteId = useCallback(
+        () => {
+            onDelete(good.id);
+        },
+        [onDelete, good.id],
+    );
 
-    onToggle = ({target}) => {
-        this.setState({
-            isActive: target.checked,
-        });
-        this.props.onToggle(this.props.good.id);
-        console.log(this.props.good.id);
-    };
+    const onToggleGood = useCallback(
+        ({target}) => {
+            setStateElement({
+                ...stateElement,
+                isActive: target.checked,
+            });
+            onToggle(good.id);
+        },
+        [onToggle, good.id, stateElement],
+    );
 
-    onEdit = () => {
-        this.setState({
-            ...this.state,
-            isEditMode: true,
-            title: this.props.good.title,
-            weight: this.props.good.weight,
-            description: this.props.good.description,
-        });
-    };
+    const onEditGood = useCallback(
+        () => {
+            setStateElement({
+                ...stateElement,
+                isEditMode: true,
+                title: good.title,
+                weight: good.weight,
+                description: good.description,
+            });
+        },
+        [good, stateElement],
+    );
 
-    onEditSave = (updatedGood) => {
-        const {title, weight, description} = this.state;
-        this.props.onEdit({...this.props.good, title, weight, description});
-        this.onEditCancel();
-    };
+    const onEditCancel = useCallback(
+        () => {
+            setStateElement({
+                ...stateElement,
+                isEditMode: false,
+            });
+        },
+        [stateElement],
+    );
 
-    onInputChange = ({target}) => {
-        this.setState({
-            [target.name]: target.value,
-        });
-    };
+    const onEditSaveGood = useCallback(
+        (updatedGood) => {
+            const {title, weight, description} = stateElement;
+            onEdit({...good, title, weight, description});
+            onEditCancel();
+        },
+        [good, stateElement, onEdit, onEditCancel],
+    );
 
-    onEditCancel = () => {
-        // exit edit mode
-        this.setState({...this.state, isEditMode: false});
-    };
+    const onInputChange = useCallback(
+        ({target}) => {
+            setStateElement({
+                ...stateElement,
+                [target.name]: target.value,
+            });
+        },
+        [stateElement],
+    );
 
-    renderCard = () => {
-        const {title, weight, description} = this.props.good;
-        const {isActive} = this.state;
+    const renderCard = () => {
+        const {title, weight, description} = good;
+        const {isActive} = stateElement;
         return (
             <div className={isActive ? 'GoodListElementCard isActive' : 'GoodListElementCard'}>
                 <div>
                     <input
                         type="checkbox"
                         name="selected"
-                        checked={this.props.isSelected}
-                        onChange={this.onToggle}
+                        checked={isSelected}
+                        onChange={onToggleGood}
                         className="GoodListElementCheckbox"
                     />
                     <h3>Title: {title}</h3>
@@ -69,15 +91,15 @@ export default class GoodsListElement extends Component {
                     <p>Description: {description}</p>
                 </div>
                 <div>
-                    <button onClick={this.onDelete} className="button GoodListElementButtonDelete">Delete</button>
-                    <button onClick={this.onEdit} className="button GoodListElementButtonEdit">Edit</button>
+                    <button onClick={onDeleteId} className="button GoodListElementButtonDelete">Delete</button>
+                    <button onClick={onEditGood} className="button GoodListElementButtonEdit">Edit</button>
                 </div>
             </div>
         );
     };
 
-    renderEditCard = () => {
-        const {title, weight, description} = this.state;
+    const renderEditCard = function() {
+        const {title, weight, description} = stateElement;
         return (
             <div className="GoodListElementCard">
                 <form>
@@ -88,17 +110,19 @@ export default class GoodsListElement extends Component {
                             placeholder="Title"
                             name="title"
                             value={title}
-                            onChange={this.onInputChange}
+                            onChange={onInputChange}
                         />
                     </div>
                     <div className="GoodsListElementWrapper">
+
+
                         <input
                             className="GoodsListElementFormInput"
                             type="number"
                             placeholder="Weight"
                             name="weight"
                             value={weight}
-                            onChange={this.onInputChange}
+                            onChange={onInputChange}
                         />
                     </div>
                     <div className="GoodsListElementWrapper">
@@ -108,31 +132,28 @@ export default class GoodsListElement extends Component {
                             placeholder="Description"
                             name="description"
                             value={description}
-                            onChange={this.onInputChange}
+                            onChange={onInputChange}
                         />
                     </div>
                 </form>
                 <div>
-                    <button onClick={this.onEditSave} className="button GoodListElementButtonSaveEdit">Save</button>
-                    <button onClick={this.onEditCancel} className="button GoodListElementButtonCancel">Cancel</button>
+                    <button onClick={onEditSaveGood} className="button GoodListElementButtonSaveEdit">Save</button>
+                    <button onClick={onEditCancel} className="button GoodListElementButtonCancel">Cancel</button>
                 </div>
             </div>
         );
     };
 
-    render() {
-        const {isEditMode} = this.state;
-        return (
-            <div className="GoodsListElement">
-                {
-                    isEditMode ?
-                        this.renderEditCard() :
-                        this.renderCard()
-                }
-            </div>
-        );
-    }
+    return (
+        <div className="GoodsListElement">
+            {
+                stateElement.isEditMode ?
+                    renderEditCard() :
+                    renderCard()
+            }     </div>
+    );
 }
+
 
 GoodsListElement.defaultProps = {
     good: {},
