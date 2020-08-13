481664cfd949ef3e7dfc19cbb7e84cf6ca3f0b07
Maria <mariachernovolos@gmail.com>
2020-08-14 00:35:54 +0300

    converted to functional component GoodListForm;

--- src/GoodsListForm/GoodsListForm.js
+++ src/GoodsListForm/GoodsListForm.js
@@ -1,80 +1,84 @@
-import React, {Component} from 'react';
+import React, {useCallback, useState} from 'react';
 import './GoodsListForm.css';
 import PropTypes from 'prop-types';
 import GoodCategorySelect from '../GoodCategorySelect/GoodCategorySelect';
 
-export default class GoodsListForm extends Component {
-    state = {
+export default function GoodsListForm(props) {
+    const {onAdd, goodsCategory} = props;
+
+    const [stateInput, setStateInput] = useState({
         title: '',
         weight: '',
         description: '',
-    };
+    });
 
-    onFormSubmit = (e) => {
-        e.preventDefault();
-        this.props.onAdd(this.state);
-        this.setState({
-            title: '',
-            weight: '',
-            description: '',
-        });
-    };
+    const onFormSubmit = useCallback(
+        (e) => {
+            e.preventDefault();
+            onAdd(stateInput);
+            setStateInput({
+                title: '',
+                weight: '',
+                description: '',
+            });
+        },
+        [onAdd, stateInput],
+    );
 
-    onInputChange = ({target}) => {
-        this.setState({
+    const onInputChange = ({target}) => {
+        setStateInput({
+            ...stateInput,
             [target.name]: target.value,
         });
     };
 
-    render() {
-        const {title, weight, description} = this.state;
-        const {goodsCategory} = this.props;
-        return (
-            <div className='GoodsListForm'>
-                <form
-                    onSubmit={this.onFormSubmit}
-                >
-                    <div className="GoodsListFormWrapper">
-                        <div className="GoodsListItem">
-                            <input
-                                className="GoodsListFormInput"
-                                type="text"
-                                placeholder="Title"
-                                name="title"
-                                value={title}
-                                onChange={this.onInputChange}
-                            />
-                            <input
-                                className="GoodsListFormInput"
-                                type="number"
-                                placeholder="Weight"
-                                name="weight"
-                                value={weight}
-                                onChange={this.onInputChange}
-                            />
-                            <input
-                                className="GoodsListFormInput"
-                                type="text"
-                                placeholder="Description"
-                                name="description"
-                                value={description}
-                                onChange={this.onInputChange}
-                            />
-                            <div>
-                                <button className="GoodsListFormButton">Add</button>
-                            </div>
-                        </div>
-                        <div className="GoodsListItem">
-                            <GoodCategorySelect
-                                goodsCategory={goodsCategory}
-                                onInputChange={this.onInputChange}
-                            />
+    const {title, weight, description} = stateInput;
+
+    return (
+        <div className='GoodsListForm'>
+            <form
+                onSubmit={onFormSubmit}
+            >
+                <div className="GoodsListFormWrapper">
+                    <div className="GoodsListItem">
+                        <input
+                            className="GoodsListFormInput"
+                            type="text"
+                            placeholder="Title"
+                            name="title"
+                            value={title}
+                            onChange={onInputChange}
+                        />
+                        <input
+                            className="GoodsListFormInput"
+                            type="number"
+                            placeholder="Weight"
+                            name="weight"
+                            value={weight}
+                            onChange={onInputChange}
+                        />
+                        <input
+                            className="GoodsListFormInput"
+                            type="text"
+                            placeholder="Description"
+                            name="description"
+                            value={description}
+                            onChange={onInputChange}
+                        />
+                        <div>
+                            <button className="GoodsListFormButton">Add</button>
                         </div>
                     </div>
-                </form>
-            </div>
-        );
-    }
+                    <div className="GoodsListItem">
+                        <GoodCategorySelect
+                            goodsCategory={goodsCategory}
+                            onInputChange={onInputChange}
+                        />
+                    </div>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 GoodsListForm.defaultProps = {
