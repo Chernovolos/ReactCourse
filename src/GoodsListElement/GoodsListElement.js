35c54dd81bb11159a0908aa3fa4fd38ac10ecf2e
Maria <mariachernovolos@gmail.com>
2020-08-14 23:31:34 +0300

    the form has been changed; added validation; propTypes changed;

--- src/GoodsListElement/GoodsListElement.js
+++ src/GoodsListElement/GoodsListElement.js
@@ -1,17 +1,33 @@
 import React, {useState, useCallback} from 'react';
-import './GoodsListElement.css';
 import PropTypes from 'prop-types';
+import {Button, Col, Form, Card} from 'react-bootstrap';
+
 
 export default function GoodsListElement(props) {
     const {good, isSelected, onDelete, onToggle, onEdit} = props;
 
     const [stateElement, setStateElement] = useState({
-        isActive: false,
         isEditMode: false,
         title: '',
         weight: '',
         description: '',
+        weightError: '',
     });
+
+    const [validated, setValidated] = useState(false);
+
+    const handleSubmit = (event) => {
+        const {title, weight, description} = stateElement;
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        setValidated(true);
+        onEdit({...good, title, weight, description});
+        onEditCancel();
+        setValidated(false);
+    };
 
     const onDeleteId = useCallback(
         () => {
@@ -20,16 +36,9 @@ export default function GoodsListElement(props) {
         [onDelete, good.id],
     );
 
-    const onToggleGood = useCallback(
-        ({target}) => {
-            setStateElement({
-                ...stateElement,
-                isActive: target.checked,
-            });
-            onToggle(good.id);
-        },
-        [onToggle, good.id, stateElement],
-    );
+    const onToggleGood = () => {
+        onToggle(good.id);
+    };
 
     const onEditGood = useCallback(
         () => {
@@ -54,15 +63,6 @@ export default function GoodsListElement(props) {
         [stateElement],
     );
 
-    const onEditSaveGood = useCallback(
-        (updatedGood) => {
-            const {title, weight, description} = stateElement;
-            onEdit({...good, title, weight, description});
-            onEditCancel();
-        },
-        [good, stateElement, onEdit, onEditCancel],
-    );
-
     const onInputChange = useCallback(
         ({target}) => {
             setStateElement({
@@ -75,108 +75,125 @@ export default function GoodsListElement(props) {
 
     const renderCard = () => {
         const {title, weight, description} = good;
-        const {isActive} = stateElement;
         return (
-            <div className={isActive ? 'GoodListElementCard isActive' : 'GoodListElementCard'}>
-                <div>
-                    <input
-                        type="checkbox"
-                        name="selected"
-                        checked={isSelected}
-                        onChange={onToggleGood}
-                        className="GoodListElementCheckbox"
-                    />
-                    <h3>Title: {title}</h3>
-                    <p>Weight: {weight}</p>
-                    <p>Description: {description}</p>
-                </div>
-                <div>
-                    <button onClick={onDeleteId} className="button GoodListElementButtonDelete">Delete</button>
-                    <button onClick={onEditGood} className="button GoodListElementButtonEdit">Edit</button>
-                </div>
-            </div>
+            <Card className={ isSelected? 'border border-primary':'border border-dark'}>
+                <Card.Header>
+                    <Form.Group controlId='selected'>
+                        <Form.Check
+                            type='checkbox'
+                            name='selected'
+                            checked={isSelected}
+                            onChange={onToggleGood}
+                        />
+                    </Form.Group>
+                </Card.Header>
+                <Card.Body>
+                    <Card.Text>Title: {title}</Card.Text>
+                    <Card.Text>Weight: {weight}</Card.Text>
+                    <Card.Text>Description: {description}</Card.Text>
+                    <Button
+                        onClick={onDeleteId}
+                        size='lg'
+                        variant='danger'
+                        className='mr-3 text-uppercase'
+                    >
+                        Delete
+                    </Button>
+                    <Button
+                        onClick={onEditGood}
+                        size='lg'
+                        variant='primary'
+                        className='text-uppercase'
+                    >
+                        Edit
+                    </Button>
+                </Card.Body>
+            </Card>
         );
     };
 
     const renderEditCard = function() {
         const {title, weight, description} = stateElement;
+
         return (
-            <div className="GoodListElementCard">
-                <form>
-                    <div className="GoodsListElementWrapper">
-                        <input
-                            className="GoodsListElementFormInput"
-                            type="text"
-                            placeholder="Title"
-                            name="title"
-                            value={title}
-                            onChange={onInputChange}
-                        />
-                    </div>
-                    <div className="GoodsListElementWrapper">
+            <Card className='border border-dark'>
+                <Card.Body>
+                    <Form noValidate validated={validated} onSubmit={handleSubmit}>
 
+                        <Form.Group controlId='title'>
+                            <Form.Control
+                                type='text'
+                                name='title'
+                                value={title}
+                                onChange={onInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
 
-                        <input
-                            className="GoodsListElementFormInput"
-                            type="number"
-                            placeholder="Weight"
-                            name="weight"
-                            value={weight}
-                            onChange={onInputChange}
-                        />
-                    </div>
-                    <div className="GoodsListElementWrapper">
-                        <input
-                            className="GoodsListElementFormInput"
-                            type="text"
-                            placeholder="Description"
-                            name="description"
-                            value={description}
-                            onChange={onInputChange}
-                        />
-                    </div>
-                </form>
-                <div>
-                    <button onClick={onEditSaveGood} className="button GoodListElementButtonSaveEdit">Save</button>
-                    <button onClick={onEditCancel} className="button GoodListElementButtonCancel">Cancel</button>
-                </div>
-            </div>
+                        <Form.Group controlId='weight'>
+                            <Form.Control
+                                required
+                                type='number'
+                                name='weight'
+                                value={weight}
+                                onChange={onInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+
+                        <Form.Group controlId='description'>
+                            <Form.Control
+                                type='text'
+                                name='description'
+                                value={description}
+                                onChange={onInputChange}
+                            />
+                        </Form.Group>
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+
+                        <Button
+                            type='submit'
+                            size='lg'
+                            className='mr-3 text-uppercase'
+                            disabled={!weight.length}
+                        >
+                            Save
+                        </Button>
+                        <Button
+                            onClick={onEditCancel}
+                            size='lg'
+                            variant='danger'
+                            className='mr-3 text-uppercase'
+                        >
+                            Cancel
+                        </Button>
+
+                    </Form>
+                </Card.Body>
+            </Card>
         );
     };
 
     return (
-        <div className="GoodsListElement">
+        <Col sm={4} className='pb-3'>
             {
                 stateElement.isEditMode ?
                     renderEditCard() :
                     renderCard()
-            }     </div>
+            }     </Col>
     );
 }
 
 
 GoodsListElement.defaultProps = {
     good: {},
-    isActive: false,
-    isEditMode: false,
     isSelected: true,
-    title: '',
-    weight: '',
-    description: '',
 };
 
 GoodsListElement.propTypes = {
     good: PropTypes.object,
-    isActive: PropTypes.bool,
-    isEditMode: PropTypes.bool,
     isSelected: PropTypes.bool,
-    title: PropTypes.string,
-    weight: PropTypes.string,
-    description: PropTypes.string,
     onDelete: PropTypes.func,
     onToggle: PropTypes.func,
     onEdit: PropTypes.func,
-    onEditCancel: PropTypes.func,
-    onEditSave: PropTypes.func,
-    onDeleteSelected: PropTypes.func,
 };
--- src/GoodsListForm/GoodsListForm.js
+++ src/GoodsListForm/GoodsListForm.js
@@ -1,7 +1,7 @@
 import React, {useCallback, useState} from 'react';
-import './GoodsListForm.css';
 import PropTypes from 'prop-types';
 import GoodCategorySelect from '../GoodCategorySelect/GoodCategorySelect';
+import {Button, Col, Form} from 'react-bootstrap';
 
 export default function GoodsListForm(props) {
     const {onAdd, goodsCategory} = props;
@@ -12,7 +12,32 @@ export default function GoodsListForm(props) {
         description: '',
     });
 
-    const onFormSubmit = useCallback(
+    const [validated, setValidated] = useState(false);
+
+    // const onFormSubmit = useCallback(
+    //     (e) => {
+    //         e.preventDefault();
+    //         onAdd(stateInput);
+    //         setStateInput({
+    //             title: '',
+    //             weight: '',
+    //             description: '',
+    //         });
+    //     },
+    //     [onAdd, stateInput],
+    // );
+
+    const onInputChange = useCallback(
+        ({target}) => {
+            setStateInput({
+                ...stateInput,
+                [target.name]: target.value,
+            });
+        },
+        [stateInput],
+    );
+
+    const onAddGood = useCallback(
         (e) => {
             e.preventDefault();
             onAdd(stateInput);
@@ -25,75 +50,81 @@ export default function GoodsListForm(props) {
         [onAdd, stateInput],
     );
 
-    const onInputChange = ({target}) => {
-        setStateInput({
-            ...stateInput,
-            [target.name]: target.value,
-        });
+    const handleSubmit = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        setValidated(true);
+        onAddGood(event);
+        setValidated(false);
     };
 
     const {title, weight, description} = stateInput;
 
     return (
-        <div className='GoodsListForm'>
-            <form
-                onSubmit={onFormSubmit}
-            >
-                <div className="GoodsListFormWrapper">
-                    <div className="GoodsListItem">
-                        <input
-                            className="GoodsListFormInput"
-                            type="text"
-                            placeholder="Title"
-                            name="title"
+        <>
+            <Col sm={4}>
+                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                    <Form.Group controlId='title'>
+                        <Form.Control
+                            type='text'
+                            placeholder='Title...'
+                            name='title'
                             value={title}
                             onChange={onInputChange}
                         />
-                        <input
-                            className="GoodsListFormInput"
-                            type="number"
-                            placeholder="Weight"
-                            name="weight"
+                    </Form.Group>
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+
+                    <Form.Group controlId='weight'>
+                        <Form.Control
+                            type='number'
+                            placeholder='Weight...'
+                            name='weight'
                             value={weight}
                             onChange={onInputChange}
                         />
-                        <input
-                            className="GoodsListFormInput"
-                            type="text"
-                            placeholder="Description"
-                            name="description"
+                    </Form.Group>
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+
+                    <Form.Group controlId='description'>
+                        <Form.Control
+                            type='description'
+                            placeholder='Description...'
+                            name='description'
                             value={description}
                             onChange={onInputChange}
                         />
-                        <div>
-                            <button className="GoodsListFormButton">Add</button>
-                        </div>
-                    </div>
-                    <div className="GoodsListItem">
-                        <GoodCategorySelect
-                            goodsCategory={goodsCategory}
-                            onInputChange={onInputChange}
-                        />
-                    </div>
-                </div>
-            </form>
-        </div>
+                    </Form.Group>
+                    <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+
+                    <Button
+                        type='submit'
+                        size='lg'
+                        className='text-uppercase'
+                        disabled={!weight.length || !description.length || !title.length}
+                    >
+                    Add
+                    </Button>
+                </Form>
+            </Col>
+            <Col sm={4}>
+                <GoodCategorySelect
+                    goodsCategory={goodsCategory}
+                    onInputChange={onInputChange}
+                />
+            </Col>
+        </>
     );
 }
 
 GoodsListForm.defaultProps = {
-    title: '',
-    weight: '',
-    description: '',
     goodsCategory: [],
 };
 
 GoodsListForm.propTypes = {
-    title: PropTypes.string,
-    weight: PropTypes.string,
-    description: PropTypes.string,
     goodsCategory: PropTypes.array,
     onAdd: PropTypes.func,
-    onFormSubmit: PropTypes.func,
-    onInputChange: PropTypes.func,
 };
